f296e68b74016444013f508b411c136e0602da0d
Rudra Prasad Nayak <rudraprasadnayak728@gmail.com>
2025-03-31 19:10:57 +0530

    Updated

--- Scatch/routes/usersRouter.js
+++ Scatch/routes/usersRouter.js
@@ -5,11 +5,6 @@ const {
   loginUser,
   logoutUser,
 } = require("../controllers/authController");
-const isLoggedIn = require("../middleware/isLoggedIn");
-
-router.get("/", (req, res) => {
-  res.send("usersRouter");
-});
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
